eb538953d8ed72b5035de03cae8459987bb473c3
Mari <88294204+Marikazia@users.noreply.github.com>
2021-11-19 22:05:10 +0100

    Add files via upload

--- unit_18.js
+++ unit_18.js
@@ -33,7 +33,13 @@ document.querySelector('.b-2').onclick = t2;
 /*  Дан массив a3 = [2, 'hello', 3, 'hi', 4, 'Mazai'] - с помощью forEach переберите массив и создайте новый массив a3_res куда добавьте элементы данного массива являющиеся числом. Запускаться решение должно при вызове функции t3.*/
 
 function t3() {
-
+    a3 = [2, 'hello', 3, 'hi', 4, 'Mazai']
+    a3.forEach(elem => {
+        if( typeof elem === 'number' ) {
+            a3_res.push(elem);
+        }
+    })
+    document.querySelector('.out-3').innerHTML = a3_res;
 }
 
 document.querySelector('.b-3').onclick = t3;
@@ -43,7 +49,12 @@ document.querySelector('.b-3').onclick = t3;
 /*  На странице созданы 3 span.task-4 c атрибутом data. С помощью forEach переберите их и добавьте атрибуты в массив a4_res  Запускаться решение должно при вызове функции t4.*/
 
 function t4() {
+    a4 = document.querySelectorAll('.task-4');
 
+    a4.forEach( elem => {
+        a4_res.push(elem.getAttribute('data'));        
+    })
+    document.querySelector('.out-4').innerHTML = a4_res;
 }
 
 document.querySelector('.b-4').onclick = t4;
@@ -53,7 +64,9 @@ document.querySelector('.b-4').onclick = t4;
 /*  На странице созданы 3 p.task-5 c атрибутом data. С помощью forEach переберите их и добавьте событие клик. Напишите функцию t5, которая будет запускаться при клике и добавлять атрибут data элемента, по которому кликнули в массив a5_res.*/
 
 function t5() {
+    a5_res.push(this.getAttribute('data'));
 
+    document.querySelector('.out-5').innerHTML = a5_res;
 }
 
 document.querySelectorAll('.task-5').forEach(elem => {
@@ -66,7 +79,10 @@ document.querySelectorAll('.task-5').forEach(elem => {
 /*  Дана строка str6='helloworld' - преобразуйте ее в массив и присвойте a6_res. Выведите на страницу. Запускаться решение должно при вызове функции t6. */
 
 function t6() {
-
+    str6 = 'helloworld';
+    a6_res = str6.split('');
+    console.log(a6_res);
+    document.querySelector('.out-6').innerHTML = a6_res;
 }
 
 document.querySelector('.b-6').onclick = t6;
@@ -76,7 +92,10 @@ document.querySelector('.b-6').onclick = t6;
 /*  Дана строка str7='hello world hi mazai' - преобразуйте ее в массив и разбейте по словам. Причем слова не должны содержать пробелов и присвойте a6_res. выведите на страницу.  Запускаться решение должно при вызове функции t7. */
 
 function t7() {
-
+    str7 = 'hello world hi mazai';
+    a7_res = str7.split(' ');
+    console.log(a7_res);
+    document.querySelector('.out-7').innerHTML = a7_res;
 }
 
 document.querySelector('.b-7').onclick = t7;
@@ -85,7 +104,10 @@ document.querySelector('.b-7').onclick = t7;
 /*  Дан массив a8 = [1,2,66,77,15] - преобразуйте ее в строку. Разделитель - дефис. Результат присвойте a8_res. Запускаться решение должно при вызове функции t8. */
 
 function t8() {
-
+    a8 = [1,2,66,77,15];
+    a8_res = a8.join('-');
+    console.log(a8_res);
+    document.querySelector('.out-8').innerHTML = a8_res;
 }
 
 document.querySelector('.b-8').onclick = t8;
@@ -95,6 +117,16 @@ document.querySelector('.b-8').onclick = t8;
 /*  Дан массив a9 = [[hi, mahai], [test, best]] - преобразуйте его в строку. Разделитель - дефис. Результат присвойте a9_res. Запускаться решение должно при вызове функции t9. Допускается лишний дефис в конце строки!!! */
 
 function t9() {
+    a9 = [['hi', 'mahai'], ['test', 'best']];
+    a9_res = a9.flat().join('-');
+    // toString(a9)
+    // na9 = Array.from(a9);
+    // a9_res = a9.forEach( elem => {
+    //     elem.join('-');
+    // })
+    // a9_res = na9.join('-');
+    console.log(a9_res);
+    document.querySelector('.out-9').innerHTML = a9_res;
 
 }
 
@@ -105,6 +137,9 @@ document.querySelector('.b-9').onclick = t9;
 /*  Дан массив a10 = {name: ivan, age: 15, sex: 1, id: 45} - преобразуйте его в query строку ( так передаются GET параметры). Найдите описание что такое query строка самостоятельно. Разделитель - амперсанд. Результат присвойте a10_res. Запускаться решение должно при вызове функции t10. Допускается лишний амперсанд в конце строки!!! */
 
 function t10() {
+    a10 = {name: ivan, age: 15, sex: 1, id: 45};
+    console.log(a10_res);
+    document.querySelector('.out-10').innerHTML = a10_res;
 
 }
 
