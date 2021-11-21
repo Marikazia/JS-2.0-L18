ebf1843f8a4f5687564bf1bca68a8fcca755e76f
Mari <88294204+Marikazia@users.noreply.github.com>
2021-11-21 16:01:29 +0100

    Update unit_18.js

--- unit_18.js
+++ unit_18.js
@@ -137,10 +137,17 @@ document.querySelector('.b-9').onclick = t9;
 /*  Дан массив a10 = {name: ivan, age: 15, sex: 1, id: 45} - преобразуйте его в query строку ( так передаются GET параметры). Найдите описание что такое query строка самостоятельно. Разделитель - амперсанд. Результат присвойте a10_res. Запускаться решение должно при вызове функции t10. Допускается лишний амперсанд в конце строки!!! */
 
 function t10() {
-    a10 = {name: ivan, age: 15, sex: 1, id: 45};
+    a10 = {name: 'ivan', age: 15, sex: 1, id: 45};
+    na10 = [];
+    // console.log(a10);
+    for (let key in a10) {
+        na10 +=  key + '=' + a10[key] + '&';
+    }
+    a10_res = '?' + na10;
     console.log(a10_res);
     document.querySelector('.out-10').innerHTML = a10_res;
-
 }
 
 document.querySelector('.b-10').onclick = t10;
+
+// ?name=ivan&age=15&sex=1&id= 45
